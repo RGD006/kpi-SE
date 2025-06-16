9dfce139db8ebb2b6fe4129d1792bee51c3c41af
RGD006 <mysak6288@gmail.com>
2025-06-16 15:58:54 +0300

    feat: add window background

--- src/Window/Window.cpp
+++ src/Window/Window.cpp
@@ -16,10 +16,10 @@ Window::Window(const char *window_title, u32 w, u32 h)
 {
     if (IMG_Init(IMG_INIT_PNG) == 0)
     {
-        std::cerr << "IMG init error " << IMG_GetError() << std::endl; 
+        std::cerr << "IMG init error " << IMG_GetError() << std::endl;
         exit(EXIT_FAILURE);
     }
-        
+
     SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "linear");
     window = SDL_CreateWindow(window_title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, w, h, 0);
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
@@ -36,6 +36,13 @@ void Window::run(void)
     bool window_run = true;
 
     SDL_Rect canvas_background = {0};
+    SDL_Rect window_background = {
+        .x = 0,
+        .y = 0,
+        .w = width,
+        .h = height,
+    };
+
     color_t color_canvas_background(0xFFFFFFFF);
 
     canvas.setBackground(color_canvas_background, &canvas_background);
@@ -45,37 +52,39 @@ void Window::run(void)
 
     Pen pen(rect);
     pen.addCanvas(&canvas);
-    
+
     auto pen_mouse_down_evnt = [&pen](void *arg)
     { pen.eventMouseDown(arg); };
     auto pen_mouse_move_evnt = [&pen](void *arg)
     { pen.eventMouseMove(arg); };
     auto pen_mouse_up_evnt = [&pen](void *arg)
     { pen.eventMouseUp(arg); };
-    
+
     Button button_save = Button(BUTTON_SAVE, createRect(0, 0, 0, 0), createRect(0, 0, 100, 100), renderer, "images/cat.png");
-    
+
     buttons.push_back(button_save);
     event_handler.addButton(button_save);
-    
+
     event_handler.addEvent(SDL_QUIT, exitWindow, reinterpret_cast<void *>(&window_run));
     event_handler.addEvent(BUTTON_SAVE, exitWindow, reinterpret_cast<void *>(&window_run));
     event_handler.addIOEvent(SDL_MOUSEBUTTONDOWN, SDL_BUTTON_LEFT, pen_mouse_down_evnt, pen.getMoveState());
     event_handler.addIOEvent(SDL_MOUSEMOTION, SDL_NO_BUTTON, pen_mouse_move_evnt, pen.getMoveState());
     event_handler.addIOEvent(SDL_MOUSEBUTTONUP, SDL_BUTTON_LEFT, pen_mouse_up_evnt, pen.getMoveState());
 
-    
     while (window_run)
     {
         SDL_SetRenderTarget(renderer, nullptr);
-        
+        // draw window background
+        SDL_SetRenderDrawColor(renderer, 0xA0, 0xA0, 0xA0, 0xFF);
+        SDL_RenderFillRect(renderer, &window_background);
+
         event_handler.run();
 
         for (auto &button : buttons)
         {
             button.render();
         }
-        
+
         canvas.render();
         SDL_RenderPresent(renderer);
     }
