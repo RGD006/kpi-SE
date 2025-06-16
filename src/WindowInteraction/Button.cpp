008f4f8a1ff900cd11387edc32d5c22e106401f0
RGD006 <mysak6288@gmail.com>
2025-06-16 16:13:44 +0300

    feat: add set button background plain color

--- src/WindowInteraction/Button.cpp
+++ src/WindowInteraction/Button.cpp
@@ -32,6 +32,14 @@ void Button::setTexture(SDL_Texture *texture)
     this->texture = texture;
 }
 
+void Button::setTexture(SDL_Rect *rect, u32 color)
+{
+    color_t rect_color(color);
+    SDL_SetRenderTarget(renderer, texture);
+    SDL_SetRenderDrawColor(renderer, rect_color.r, rect_color.g, rect_color.b, rect_color.a);
+    SDL_RenderFillRect(renderer, rect);
+}
+
 void Button::setTexture(const char *path)
 {
     texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, s_rect.w, s_rect.h);
@@ -53,12 +61,11 @@ void Button::render()
     {
         SDL_RenderCopy(renderer, texture, &s_rect, &d_rect);
     }
-
 }
 
 void Button::listenMouse(Mouse &mouse)
 {
-    if (SDL_HasIntersection(&d_rect, reinterpret_cast<SDL_Rect*>(mouse.releaseLeft(nullptr))) == SDL_TRUE)
+    if (SDL_HasIntersection(&d_rect, reinterpret_cast<SDL_Rect *>(mouse.releaseLeft(nullptr))) == SDL_TRUE)
     {
         std::cout << "Button is selected!" << std::endl;
         SDL_PushEvent(&event);
